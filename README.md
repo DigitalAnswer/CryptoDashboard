55287f51556f000569d0ba80eed85b79183d490b
Ben <benjamin@prieur.org>
2017-11-08 23:23:17 +0100

    Create db and few sql requests

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
+## third party dependencies
+```go
+go get github.com/go-sql-driver/mysql
+go get golang.org/x/crypto/bcrypt
+```
+
 ## DOCKER Container
 ### mySQL
 ``` shell
 docker run -d --name mysql-dev -e MYSQL_ROOT_PASSWORD=admin --publish 6603:3306 --mount type=bind,source="$(pwd)"/datadir,target=/var/lib/mysql mysql
+```
+
+``` sql
+DROP TABLE IF EXISTS "currency_info";
+CREATE TABLE `currency_info` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `code` varchar(10) NOT NULL DEFAULT '',
+  `name` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS "btc_prices";
+CREATE TABLE `btc_prices` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `price_usd` double DEFAULT NULL,
+  `price_btc` double DEFAULT NULL,
+  `PercentChange24h` double DEFAULT NULL,
+  `currency_code` varchar(10) NOT NULL DEFAULT '',
+  `last_update` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `time` (`last_update`,`currency_code`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 ```
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -20,6 +21,8 @@ var settings = Settings{
 var ethWallet = "0xb837521FeE201B36bDAcdA38f6c0df97E0Cb6e9E"
 
 func main() {
+
+	// http.ListenAndServe(":8080", nil)
 
 	log.Printf(settings.currency)
 
@@ -46,5 +49,55 @@ func main() {
 	err = db.Ping()
 	if err != nil {
 		panic(err.Error())
+	}
+
+	listAllCrypto()
+
+	_, err = db.Exec("INSERT INTO btc_prices(last_update, currency_code, price_usd, price_btc, PercentChange24h) VALUES(?, ?, ?, ?, ?)", result.LastUpdated, result.Symbol, result.PriceUSD, result.PriceBTC, result.PercentChange24h)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	listAllData()
+}
+
+func listAllCrypto() {
+
+	// Grab from the database
+	var databaseCode string
+	var databaseName string
+
+	err := db.QueryRow("SELECT code, name FROM currency_info WHERE code='BTC'").Scan(&databaseCode, &databaseName)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Printf("result db: %s-%s", databaseCode, databaseName)
+}
+
+func listAllData() {
+
+	rows, err := db.Query("SELECT COUNT(*) as count FROM btc_prices")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer rows.Close()
+
+	var count int
+	for rows.Next() {
+		err := rows.Scan(&count)
+		checkErr(err)
+	}
+
+	fmt.Println("Total count:", count)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
